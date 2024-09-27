b9cff777202f75c70d33e1d8f32bbf1a3c7f8a12
Panupong Boom Utvichai <boompanupong@gmail.com>
2024-09-27 22:19:42 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,135 @@
+# Up Down Left Right - Practice Program
+
+#TODO: Voice Engine + Math Interuption
+
+import random
+
+def draw_final_answer(total_command):
+  block = [
+    ["L0 R0", "L0 R0", "L0 R0", "L0 R0"],
+    ["L0 R0", "L0 R0", "L0 R0", "L0 R0"],
+    ["L0 R0", "L0 R0", "L0 R0", "L0 R0"],
+    ["L0 R0", "L0 R0", "L0 R0", "L0 R0"]
+  ]
+
+  for command in total_command:
+    print("command: " + command)
+    compute_answer(command, block)
+
+  count = 0
+  print()
+  print("======================ANSWER======================\n")
+  while count < 4:
+     print(block[count])
+     print()
+     count += 1
+
+def compute_answer(command, answer_block):
+   # Command come as "UDLRL"
+   x_value = 0
+   y_value = 0
+
+   # BASED on Main Direction (1st Alphabet)
+   if (command[0] == "U" or command[0] == "D"):
+      # Start Vertically
+      if (command[0] == "U"):
+         x_value = 0
+      else:
+         x_value = 3
+
+      if (command[1] == "U" and command[0] == "U"):
+         x_value += 0
+      elif (command[1] == "D" and command[0] == "D"):
+         x_value += 0
+      elif (command[1] == "U" and x_value == 3):
+         x_value -= 1
+      else:
+         x_value += 1
+
+      if (command[2] == "L"):
+         y_value = 0
+      else:
+         y_value = 3
+
+      if (command[3] == "L" and command[2] == "L"):
+         y_value += 0
+      elif (command[3] == "R" and command[2] == "R"):
+         y_value += 0
+      elif (command[3] == "L" and y_value == 3):
+         y_value -= 1
+      else:
+         y_value += 1
+
+   else:
+      # Start Horizontally
+      if (command[0] == "L"):
+         y_value = 0
+      else:
+         y_value = 3
+
+      if (command[1] == "L" and command[0] == "L"):
+         y_value += 0
+      elif (command[1] == "R" and command[0] == "R"):
+         y_value += 0
+      elif (command[1] == "L" and y_value == 3):
+         y_value -= 1
+      else:
+         y_value += 1
+
+      if (command[2] == "U"):
+         x_value = 0
+      else:
+         x_value = 3
+
+      if (command[3] == "U" and command[2] == "U"):
+         x_value += 0
+      elif (command[3] == "D" and command[2] == "D"):
+         x_value += 0
+      elif (command[3] == "U" and x_value == 3):
+         x_value -= 1
+      else:
+         x_value += 1
+   
+   print("XY: " + str(x_value) + str(y_value) + " | L/R: " + str(command[4].upper()))
+
+   target_block = answer_block[x_value][y_value]
+   hand_to_write = command[4]
+   split_left_right_count = target_block.split(" ")
+   left_count = int(split_left_right_count[0][1])
+   right_count = int(split_left_right_count[1][1])
+   if hand_to_write == "L":
+       left_count += 1
+   else:
+       right_count += 1
+   answer_block[x_value][y_value] = "L" + str(left_count) + " R" + str(right_count)
+
+# START
+choices_start_direction = ["updown", "leftright"]
+choices_up_down = ["U", "D"]
+choices_left_right = ["L", "R"]
+
+print("random start")
+
+total_command = []
+x = 1
+while x <= 20:
+    start_direction = random.choices(choices_start_direction)
+    if (start_direction[0] == "updown") :
+        alphabet1 = random.choice(choices_up_down)
+        alphabet2 = random.choice(choices_up_down)
+        alphabet3 = random.choice(choices_left_right)
+        alphabet4 = random.choice(choices_left_right)
+        alphabet5 = random.choice(choices_left_right)
+    else:
+        alphabet1 = random.choice(choices_left_right)
+        alphabet2 = random.choice(choices_left_right)
+        alphabet3 = random.choice(choices_up_down)
+        alphabet4 = random.choice(choices_up_down)
+        alphabet5 = random.choice(choices_left_right)
+    command = alphabet1[0]+alphabet2[0]+alphabet3[0]+alphabet4[0]+alphabet5[0]
+    total_command.append(command)
+    x += 1
+    
+print(total_command)
+print(len(total_command))
+draw_final_answer(total_command)
