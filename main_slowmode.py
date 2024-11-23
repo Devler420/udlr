80b9d3bc651d38527a43589cbb071774845fa1f0
Boom P <boompanupong@gmail.com>
2024-11-23 16:50:00 +0700

    add cp

--- main_slowmode.py
+++ main_slowmode.py
@@ -1,6 +1,16 @@
-# Up Down Left Right - Practice Program
-# Required Internet on this Branch (gTTS)
-# CopyRight by Panupong Utvichai
+# Copyright (c) [2024] [Panupong Utvichai]
+
+# Licensed under the Mozilla Public License Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://mozilla.org/MPL/2.0/
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.   
 
 import random
 from gtts import gTTS
